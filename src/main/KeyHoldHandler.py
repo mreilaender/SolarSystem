09249a1611f584f6b519835fa57fde9145f80f58
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-08 21:47:03 +0100

    Bug fix

---
+++ src/main/KeyHoldHandler.py
@@ -0,0 +1,38 @@
+from threading import Thread
+
+from pyglet.window import key
+
+
+class KeyHoldHandler(Thread):
+
+    def __init__(self, key_state_handler, camera):
+        super().__init__()
+        self.key_state_handler = key_state_handler
+        self.camera = camera
+
+    def run(self):
+        factor = 0.00003
+        while self.key_state_handler[key.W]:
+            self.camera.z -= factor
+        while self.key_state_handler[key.S]:
+            self.camera.z += factor
+        while self.key_state_handler[key.A]:
+            self.camera.x -= factor
+        while self.key_state_handler[key.D]:
+            self.camera.x += factor
+
+        while self.key_state_handler[key.UP]:
+            self.camera.reset()
+            self.camera.rotatex = 1
+            self.camera.angle += factor
+        while self.key_state_handler[key.DOWN]:
+            self.camera.reset()
+            self.camera.rotatex = 1
+            self.camera.angle -= factor
+        while self.key_state_handler[key.LEFT]:
+            self.camera.reset()
+            self.camera.rotatey = 1
+            self.camera.angle += factor
+        while self.key_state_handler[key.RIGHT]:
+            self.camera.rotatey = 1
+            self.camera.angle -= factor
