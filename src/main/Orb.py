df9e376290d981c811093c4bdfc6b55c6e5b7137
BuildTools <mreilaender@student.tgm.ac.at>
2015-12-10 21:29:08 +0100

    Texture implemented

--- src/main/Orb.py
+++ src/main/Orb.py
@@ -1,17 +1,15 @@
-import pyglet
 from abc import *
 
+import pyglet
 from pyglet.gl.gl import *
 from pyglet.gl.glu import *
-
 __author__ = 'mreilaener'
 
 
 class Orb:
-    # What does metaclass mean? Super class?
     __metaclass__ = ABCMeta
 
-    def __init__(self, name, radius, surface, year_scale=1, day_scale=1, rotation_cw=True):
+    def __init__(self, name, radius, surface=False, year_scale=1, day_scale=1, rotation_cw=True):
 
         self.pos_x, self.pos_y, self.pos_z = 0, 0, 0
         self.cur_rotation_angle_year = 0
@@ -22,19 +20,16 @@ class Orb:
 
         self.name = name
         self.radius = radius
+
         self.surface = surface
+
         self.year_scale = year_scale
         self.day_scale = day_scale
         self.rotation_cw = rotation_cw
 
-        self.q = gluNewQuadric()
-        # self.surface = pyglet.image.load("../resource/example.png")
-        # self.texture = self.surface.get_texture()
-        # glEnable(GL_TEXTURE_2D)
-        # glEnable(GL_TEXTURE_2D)
-        # self.im = pyglet.image.load("../resource/earth.jpg").get_image_data()
-        # self.texture_id = GLuint(0)
-        # glGenTextures(1, self.texture_id)
+        self.sphere = gluNewQuadric()
+        gluQuadricNormals(self.sphere, GLU_SMOOTH)
+        gluQuadricTexture(self.sphere, GL_TRUE)
 
     def add_orb(self, orb):
 
@@ -71,8 +66,12 @@ class Orb:
 
         # Textures
         # TODO
-        gluSphere(self.q, self.radius, 20, 12)
-        # glBindTexture(self.texture.target, self.texture.id)
+        if self.surface is False:
+            gluSphere(self.sphere, self.radius, 512, 512)
+        else:
+            self.surface.draw()
+            gluSphere(self.sphere, self.radius, 20, 12)
+            glDisable(self.surface.texture.target)
 
         for orb in self.system:
             orb.draw()
@@ -92,8 +91,30 @@ class Star(Orb):
 
 
 class Surface(object):
+    def __init__(self, texture_path=None):
+        self.texture_path = texture_path
+        self.image = pyglet.resource.image(self.texture_path)
+        self.texture = self.image.texture
+        self._verify('width')
+        self._verify('height')
 
-    def __init__(self, size, pattern, circle):
-        self.size = size
-        self.pattern = pattern
-        self.circle = circle
+    def draw(self):
+        glEnable(self.texture.target)
+        glBindTexture(self.texture.target, self.texture.id)
+
+    def _verify(self, dimension):
+        """
+        Verifies the size of the texture. Texture needs to be to the power of 2
+
+        :param dimension: Which dimension to check (width, height)
+        :type dimension: str
+        :raise Exception: Image is not a power of two
+        """
+        value = self.texture.__getattribute__(dimension)
+        while value > 1:
+            div_float = float(value) / 2.0
+            div_int = int(div_float)
+            if not (div_float == div_int):
+                raise Exception('image %s is %d, which is not a power of 2' % (
+                    dimension, self.texture.__getattribute__(dimension)))
+            value = div_int
